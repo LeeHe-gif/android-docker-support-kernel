eb99b5084c4afc1f6f9eb1f85e1093f2adc75457
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-10-17 19:28:26 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -1,26 +1,39 @@
 #!/bin/bash
 echo "=== 步骤1: 设置proton-clang 13.0.0 ==="
+cd ~
 git clone --depth 1 https://github.com/kdrag0n/proton-clang.git
-export PATH="$(pwd)/proton-clang/bin:$PATH"
+echo "=======检查cc：======="
+~/proton-clang-20210522/bin/clang -v
+
+echo "=======检查ar：======="
+~/proton-clang-20210522/bin/llvm-ar --version
+
+echo "=======检查nm：======="
+~/proton-clang-20210522/bin/llvm-nm --version
+
+echo "=======查objcopy：======="
+~/proton-clang-20210522/bin/llvm-objcopy --version
+
+echo "=======检查objdump：======="
+~/proton-clang-20210522/bin/llvm-objdump --version
+
+echo "=======检查strip：======="
+~/proton-clang-20210522/bin/llvm-strip --version
+
+echo "=======检查aarch64-linux-gnu-gcc：======="
+aarch64-linux-gnu-gcc -v
+
+echo "=======检查arm-linux-gnueabi-gcc：======="
+arm-linux-gnueabi-gcc -v
+
 # 验证编译器是否存在
-if [ ! -f "proton-clang/bin/clang" ]; then
+if [ ! -f "~/proton-clang/bin/clang" ]; then
     echo "❌ 错误：clang编译器未找到"
     echo "请检查proton-clang是否正确下载和解压"
     exit 1
 fi
 
-# 验证编译器版本
-clang --version || {
-    echo "❌ 错误：clang无法执行"
-    exit 1
-}
-
-cd proton-clang/bin
-echo "-----------------"
-ls -l 
-echo "-----------------"
-sudo chmod 755 *
-cd ../..
+cd ~
 
 echo "=== 步骤2: 清理 ==="
 make clean
@@ -32,16 +45,17 @@ make O=out alioth_defconfig
 
 echo "=== 步骤4: 开始编译 ==="
 make -j$(nproc --all) O=out \
-    CC=proton-clang/bin/clang \
-    AR=proton-clang/bin/llvm-ar \
-    NM=proton-clang/bin/llvm-nm \
-    OBJCOPY=proton-clang/bin/llvm-objcopy \
-    OBJDUMP=proton-clang/bin/llvm-objdump \
-    STRIP=proton-clang/bin/llvm-strip \
+    ARCH=arm64 \
+    CC=~/proton-clang-20210522/bin/clang \
+    AR=~/proton-clang-20210522/bin/llvm-ar \
+    NM=~/proton-clang-20210522/bin/llvm-nm \
+    OBJCOPY=~/proton-clang-20210522/bin/llvm-objcopy \
+    OBJDUMP=~/proton-clang-20210522/bin/llvm-objdump \
+    STRIP=~/proton-clang-20210522/bin/llvm-strip \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-    KCFLAGS="-Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang"
-
+    KCFLAGS="-Wformat -Wsometimes-uninitialized -Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang" 
+    
 echo "=== 步骤5：检查编译结果 ==="
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ]; then
     echo "✅ 内核编译成功"
