139ee7dd1d79674ab3721096f3647029ffbb4bc3
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-10-17 13:23:00 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+echo "=== 步骤1: 设置proton-clang 13.0.0 ==="
+git clone -b 12 --depth 1 https://github.com/kdrag0n/proton-clang.git
 echo "=== 步骤2: 清理 ==="
 make clean
 make mrproper 
@@ -9,12 +11,12 @@ make O=out polaris_defconfig
 echo "=== 步骤4: 开始编译 ==="
 make -j$(nproc --all) O=out \
     ARCH=arm64 \
-    CC=proton-clang-20210522/bin/clang \
-    AR=proton-clang-20210522/bin/llvm-ar \
-    NM=proton-clang-20210522/bin/llvm-nm \
-    OBJCOPY=proton-clang-20210522/bin/llvm-objcopy \
-    OBJDUMP=proton-clang-20210522/bin/llvm-objdump \
-    STRIP=proton-clang-20210522/bin/llvm-strip \
+    CC=proton-clang/bin/clang \
+    AR=proton-clang/bin/llvm-ar \
+    NM=proton-clang/bin/llvm-nm \
+    OBJCOPY=proton-clang/bin/llvm-objcopy \
+    OBJDUMP=proton-clang/bin/llvm-objdump \
+    STRIP=proton-clang/bin/llvm-strip \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
     KCFLAGS="-Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang"
