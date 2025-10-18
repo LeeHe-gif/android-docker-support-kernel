d8882806e3e8a122b653d1bcf06085f6bb635798
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-10-18 13:31:22 +0800

    Update build3.sh

--- build3.sh
+++ build3.sh
@@ -47,7 +47,7 @@ make -j$(nproc --all) O=out \
     STRIP=~/proton-clang-20210522/bin/llvm-strip \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-    KCFLAGS="-Wno-array-bounds -Wformat -Wsometimes-uninitialized -Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang"
+    KCFLAGS="-Wmacro-redefined -Wno-array-bounds -Wformat -Wsometimes-uninitialized -Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang"
     
 echo "=== 步骤4：检查编译结果 ==="
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ]; then
