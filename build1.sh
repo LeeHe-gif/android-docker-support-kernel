31d8cf98c7175bd2e7bbfe9af630deae749bdb35
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-10-24 22:58:50 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -1,27 +1,29 @@
 #!/bin/bash
 echo "=== 步骤1: 设置proton-clang 13.0.0 ==="
-git clone --depth 1 https://github.com/AOSP-12/prebuilts_clang_host_linux-x86_clang-r445002.git ~/prebuilts_clang_host_linux-x86_clang-r445002
+cd ~
+wget https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/heads/main/clang-r536225.tar.gz
+tar -xf clang-r536225.tar.gz -C clang-r536225
 
 echo "=======检查cc：======="
-~/prebuilts_clang_host_linux-x86_clang-r445002/bin/clang -v
+~/clang-r536225/bin/clang -v
 
 echo "=======检查ar：======="
-~/prebuilts_clang_host_linux-x86_clang-r445002/bin/llvm-ar --version
+~/clang-r536225/bin/llvm-ar --version
 
 echo "=======检查nm：======="
-~/prebuilts_clang_host_linux-x86_clang-r445002/bin/llvm-nm --version
+~/clang-r536225/bin/llvm-nm --version
 
 echo "=======检查ld：======="
-~/prebuilts_clang_host_linux-x86_clang-r445002/bin/ld.lld -v
+~/clang-r536225/bin/ld.lld -v
 
 echo "=======查objcopy：======="
-~/prebuilts_clang_host_linux-x86_clang-r445002/bin/llvm-objcopy --version
+~/clang-r536225/bin/llvm-objcopy --version
 
 echo "=======检查objdump：======="
-~/prebuilts_clang_host_linux-x86_clang-r445002/bin/llvm-objdump --version
+~/clang-r536225/bin/llvm-objdump --version
 
 echo "=======检查strip：======="
-~/prebuilts_clang_host_linux-x86_clang-r445002/bin/llvm-strip --version
+~/clang-r536225/bin/llvm-strip --version
 
 echo "=======检查aarch64-linux-gnu-gcc：======="
 aarch64-linux-gnu-gcc -v
@@ -32,19 +34,20 @@ arm-linux-gnueabi-gcc -v
 echo "====================检查环境结束==================="
 
 echo "=== 步骤2: 配置内核 ==="
+cd /home/runner/work/android-docker-support-kernel/android-docker-support-kernel/android_kernel_xiaomi_alioth
 mkdir out
 cp arch/arm64/configs/my_alioth_defconfig out/.config
 
 echo "=== 步骤3: 开始编译 ==="
 make -j$(nproc --all) O=out \
     ARCH=arm64 \
-    CC=~/prebuilts_clang_host_linux-x86_clang-r445002/bin/clang \
-    AR=~/prebuilts_clang_host_linux-x86_clang-r445002/bin/llvm-ar \
-    NM=~/prebuilts_clang_host_linux-x86_clang-r445002/bin/llvm-nm \
-    LD=~/prebuilts_clang_host_linux-x86_clang-r445002/bin/ld.lld \
-    OBJCOPY=~/prebuilts_clang_host_linux-x86_clang-r445002/bin/llvm-objcopy \
-    OBJDUMP=~/prebuilts_clang_host_linux-x86_clang-r445002/bin/llvm-objdump \
-    STRIP=~/prebuilts_clang_host_linux-x86_clang-r445002/bin/llvm-strip \
+    CC=~/clang-r536225/bin/clang \
+    AR=~/clang-r536225/bin/llvm-ar \
+    NM=~/clang-r536225/bin/llvm-nm \
+    LD=~/clang-r536225/bin/ld.lld \
+    OBJCOPY=~/clang-r536225/bin/llvm-objcopy \
+    OBJDUMP=~/clang-r536225/bin/llvm-objdump \
+    STRIP=~/clang-r536225/bin/llvm-strip \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
     KCFLAGS="-Wno-array-bounds -Wformat -Wsometimes-uninitialized -Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang"
