fbec2eed244831b546aaec1bcb0b6be3ec82b1a6
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-10-25 12:13:02 +0800

    Update build1.sh

--- build1.sh
+++ build1.sh
@@ -51,7 +51,7 @@ make -j$(nproc --all) O=out \
     STRIP=~/clang-r536225/bin/llvm-strip \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-    KCFLAGS="-Wno-array-bounds -Wformat -Wsometimes-uninitialized -Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign -enable-trivial-auto-var-init-zero-knowing-it-will-be-removed-from-clang"
+    KCFLAGS="-Wno-array-bounds -Wformat -Wsometimes-uninitialized -Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign"
     
 echo "=== 步骤4：检查编译结果 ==="
 if [ -f "out/arch/arm64/boot/Image.gz-dtb" ]; then
