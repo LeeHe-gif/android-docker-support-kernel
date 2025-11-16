d7cb3d068d9003f389439575ac53bf7c2af8023d
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-11-16 20:59:44 +0800

    Update build4.sh

--- scripts/build4.sh
+++ scripts/build4.sh
@@ -1,54 +1,35 @@
 #!/bin/bash
-echo "=== 步骤1: 设置 clang 12.0.1 ==="
-cd ~
-wget -q https://android.googlesource.com/platform/prebuilts/clang/host/linux-x86/+archive/refs/tags/android-12.1.0_r26/clang-r416183b.tar.gz
-mkdir clang-r416183b
-tar -xf clang-r416183b.tar.gz -C clang-r416183b
-
+echo "=== 步骤1: 检查工具链 ==="
 echo "=======检查cc：======="
-~/clang-r416183b/bin/clang -v
-
+~/clang-r365631c/bin/clang -v | grep "Android"
 echo "=======检查ar：======="
-~/clang-r416183b/bin/llvm-ar --version
-
+~/clang-r365631c/bin/llvm-ar --version | grep "LLVM version"
 echo "=======检查nm：======="
-~/clang-r416183b/bin/llvm-nm --version
-
+~/clang-r365631c/bin/llvm-nm --version | grep "LLVM version"
 echo "=======检查ld：======="
-~/clang-r416183b/bin/ld.lld -v
-
+~/clang-r365631c/bin/ld.lld -v
 echo "=======查objcopy：======="
-~/clang-r416183b/bin/llvm-objcopy --version
-
+~/clang-r365631c/bin/llvm-objcopy --version | grep "LLVM version"
 echo "=======检查objdump：======="
-~/clang-r416183b/bin/llvm-objdump --version
-
+~/clang-r365631c/bin/llvm-objdump --version | grep "LLVM version"
 echo "=======检查strip：======="
-~/clang-r416183b/bin/llvm-strip --version
-
+~/clang-r365631c/bin/llvm-strip --version | grep "LLVM version"
 echo "=======检查aarch64-linux-gnu-gcc：======="
-aarch64-linux-gnu-gcc -v
-
+arm-linux-gnueabi-gcc -v 2>&1 | grep "gcc version"
 echo "=======检查arm-linux-gnueabi-gcc：======="
-arm-linux-gnueabi-gcc -v
-
+arm-linux-gnueabi-gcc -v 2>&1 | grep "gcc version"
 echo "====================检查环境结束==================="
 
-echo "=== 步骤2: 配置内核 ==="
-cd /home/runner/work/android-docker-support-kernel/android-docker-support-kernel/kernel_source
-mkdir out
-cp arch/arm64/configs/test_U30Air_defconfig out/.config
-
-echo "=== 步骤3: 开始编译 ==="
+echo "=== 步骤2: 开始编译 ==="
 make -j$(nproc --all) O=out \
     ARCH=arm64 \
-    CC=~/clang-r416183b/bin/clang \
-    AR=~/clang-r416183b/bin/llvm-ar \
-    NM=~/clang-r416183b/bin/llvm-nm \
-    LD=~/clang-r416183b/bin/ld.lld \
-    OBJCOPY=~/clang-r416183b/bin/llvm-objcopy \
-    OBJDUMP=~/clang-r416183b/bin/llvm-objdump \
-    STRIP=~/clang-r416183b/bin/llvm-strip \
+    CC=~/clang-r365631c/bin/clang \
+    AR=~/clang-r365631c/bin/llvm-ar \
+    NM=~/clang-r365631c/bin/llvm-nm \
+    LD=~/clang-r365631c/bin/ld.lld \
+    OBJCOPY=~/clang-r365631c/bin/llvm-objcopy \
+    OBJDUMP=~/clang-r365631c/bin/llvm-objdump \
+    STRIP=~/clang-r365631c/bin/llvm-strip \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
     KCFLAGS="-Wno-array-bounds -Wformat -Wsometimes-uninitialized -Wformat-extra-args -Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign"
