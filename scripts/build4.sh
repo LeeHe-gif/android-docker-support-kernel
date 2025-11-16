cb8ddf84f9b33f497e9f07b83b4483aa1b32c7ec
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-11-16 23:18:30 +0800

    Update build4.sh

--- scripts/build4.sh
+++ scripts/build4.sh
@@ -1,26 +1,4 @@
 #!/bin/bash
-echo "=== 步骤1: 检查工具链 ==="
-echo "=======检查cc：======="
-~/clang-r365631c/bin/clang -v | grep "Android"
-echo "=======检查ar：======="
-~/clang-r365631c/bin/llvm-ar --version | grep "LLVM version"
-echo "=======检查nm：======="
-~/clang-r365631c/bin/llvm-nm --version | grep "LLVM version"
-echo "=======检查ld：======="
-~/clang-r365631c/bin/ld.lld -v
-echo "=======查objcopy：======="
-~/clang-r365631c/bin/llvm-objcopy --version | grep "LLVM version"
-echo "=======检查objdump：======="
-~/clang-r365631c/bin/llvm-objdump --version | grep "LLVM version"
-echo "=======检查strip：======="
-~/clang-r365631c/bin/llvm-strip --version | grep "LLVM version"
-echo "=======检查aarch64-linux-gnu-gcc：======="
-arm-linux-gnueabi-gcc -v 2>&1 | grep "gcc version"
-echo "=======检查arm-linux-gnueabi-gcc：======="
-arm-linux-gnueabi-gcc -v 2>&1 | grep "gcc version"
-echo "====================检查环境结束==================="
-
-echo "=== 步骤2: 开始编译 ==="
 make -j$(nproc --all) O=out \
     ARCH=arm64 \
     CC=~/clang-r365631c/bin/clang \
@@ -32,9 +10,9 @@ make -j$(nproc --all) O=out \
     STRIP=~/clang-r365631c/bin/llvm-strip \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
-    KCFLAGS="-Wno-array-bounds -Wformat -Wsometimes-uninitialized -Wformat-extra-args -Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign"
+    KCFLAGS="-Wno-error=implicit-function-declaration -Wno-array-bounds -Wformat -Wsometimes-uninitialized -Wformat-extra-args -Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign"
     
-echo "=== 步骤4：检查编译结果 ==="
+echo "=== 检查编译结果 ==="
 if [ -f "out/arch/arm64/boot/Image.gz" ]; then
     echo "✅ 内核编译成功"
 else
