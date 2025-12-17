63bbdb5de6c8620ab15b54edb52983e94fed75ee
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-12-17 21:46:58 +0800

    更新 build7.sh

--- scripts/build7.sh
+++ scripts/build7.sh
@@ -7,8 +7,8 @@ make O=out ARCH=arm64 my_xaga_defconfig \
     OBJCOPY=~/ToolChains/gcc-arm-10.3/aarch64-none-linux-gnu/bin/objcopy \
     OBJDUMP=~/ToolChains/gcc-arm-10.3/aarch64-none-linux-gnu/bin/objdump \
     STRIP=~/ToolChains/gcc-arm-10.3/aarch64-none-linux-gnu/bin/strip \
-    CROSS_COMPILE=~/ToolChains/gcc-arm-10.3/bin/aarch64-none-linux-gnu-
-
+        CROSS_COMPILE=aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 echo "=== 步骤3: 开始编译内核 ==="
 make -j$(nproc --all) O=out \
     ARCH=arm64 \
@@ -18,8 +18,8 @@ make -j$(nproc --all) O=out \
     OBJCOPY=~/ToolChains/gcc-arm-10.3/aarch64-none-linux-gnu/bin/objcopy \
     OBJDUMP=~/ToolChains/gcc-arm-10.3/aarch64-none-linux-gnu/bin/objdump \
     STRIP=~/ToolChains/gcc-arm-10.3/aarch64-none-linux-gnu/bin/strip \
-    CROSS_COMPILE=~/ToolChains/gcc-arm-10.3/bin/aarch64-none-linux-gnu-
-
+        CROSS_COMPILE=aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 echo "=== 步骤4：检查编译结果 ==="
 if [ -f "out/arch/arm64/boot/Image.gz" ]; then
     echo "✅ 内核编译成功"
