864a5c6ba7fe4ef7bd784493b888c87c50a6e73c
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2025-12-20 00:45:24 +0800

    更新 build7.sh

--- scripts/build7.sh
+++ scripts/build7.sh
@@ -2,26 +2,26 @@
 
 make O=out ARCH=arm64 my_xaga_defconfig \
     ARCH=arm64 \
-    CC=~/clang-r416183b/bin/clang \
-    AR=~/clang-r416183b/bin/llvm-ar \
-    NM=~/clang-r416183b/bin/llvm-nm \
-    LD=~/clang-r416183b/bin/ld.lld \
-    OBJCOPY=~/clang-r416183b/bin/llvm-objcopy \
-    OBJDUMP=~/clang-r416183b/bin/llvm-objdump \
-    STRIP=~/clang-r416183b/bin/llvm-strip \
+    CC=~/clang-r416183b1/bin/clang \
+    AR=~/clang-r416183b1/bin/llvm-ar \
+    NM=~/clang-r416183b1/bin/llvm-nm \
+    LD=~/clang-r416183b1/bin/ld.lld \
+    OBJCOPY=~/clang-r416183b1/bin/llvm-objcopy \
+    OBJDUMP=~/clang-r416183b1/bin/llvm-objdump \
+    STRIP=~/clang-r416183b1/bin/llvm-strip \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
 echo "=== 开始编译 ==="
 make -j$(nproc --all) O=out \
     ARCH=arm64 \
-    CC=~/clang-r416183b/bin/clang \
-    AR=~/clang-r416183b/bin/llvm-ar \
-    NM=~/clang-r416183b/bin/llvm-nm \
-    LD=~/clang-r416183b/bin/ld.lld \
-    OBJCOPY=~/clang-r416183b/bin/llvm-objcopy \
-    OBJDUMP=~/clang-r416183b/bin/llvm-objdump \
-    STRIP=~/clang-r416183b/bin/llvm-strip \
+    CC=~/clang-r416183b1/bin/clang \
+    AR=~/clang-r416183b1/bin/llvm-ar \
+    NM=~/clang-r416183b1/bin/llvm-nm \
+    LD=~/clang-r416183b1/bin/ld.lld \
+    OBJCOPY=~/clang-r416183b1/bin/llvm-objcopy \
+    OBJDUMP=~/clang-r416183b1/bin/llvm-objdump \
+    STRIP=~/clang-r416183b1/bin/llvm-strip \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
     KCFLAGS="-Wno-array-bounds -Wno-format -Wno-format-security -Wsometimes-uninitialized -Wno-format-extra-args -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign -Wno-incompatible-pointer-types"
