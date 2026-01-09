6c33a8e72557f065143fbaf835ac60f1b207ed8c
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2026-01-09 19:14:55 +0800

    Update build8.sh

--- scripts/build8.sh
+++ scripts/build8.sh
@@ -1,26 +1,29 @@
 #!/bin/bash
 
+export CLANG_PATH=/home/runner/clang-r383902/bin
+export PATH=$CLANG_PATH:$PATH
+
 make O=out my_nabu_defconfig \
     ARCH=arm64 \
-    CC=~/clang-r383902/bin/clang \
-    AR=~/clang-r383902/bin/llvm-ar \
-    NM=~/clang-r383902/bin/llvm-nm \
-    LD=~/clang-r383902/bin/ld.lld \
-    OBJCOPY=~/clang-r383902/bin/llvm-objcopy \
-    OBJDUMP=~/clang-r383902/bin/llvm-objdump \
-    STRIP=~/clang-r383902/bin/llvm-strip \
+    CC=clang \
+    AR=llvm-ar \
+    NM=llvm-nm \
+    LD=ld.lld \
+    OBJCOPY=llvm-objcopy \
+    OBJDUMP=llvm-objdump \
+    STRIP=llvm-strip \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
 make -j$(nproc --all) O=out \
     ARCH=arm64 \
-    CC=~/clang-r383902/bin/clang \
-    AR=~/clang-r383902/bin/llvm-ar \
-    NM=~/clang-r383902/bin/llvm-nm \
-    LD=~/clang-r383902/bin/ld.lld \
-    OBJCOPY=~/clang-r383902/bin/llvm-objcopy \
-    OBJDUMP=~/clang-r383902/bin/llvm-objdump \
-    STRIP=~/clang-r383902/bin/llvm-strip \
+    CC=clang \
+    AR=llvm-ar \
+    NM=llvm-nm \
+    LD=ld.lld \
+    OBJCOPY=llvm-objcopy \
+    OBJDUMP=llvm-objdump \
+    STRIP=llvm-strip \
     CROSS_COMPILE=aarch64-linux-gnu- \
     CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
     KCFLAGS="-Wno-array-bounds -Wformat -Wsometimes-uninitialized -Wformat-extra-args -Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign"
