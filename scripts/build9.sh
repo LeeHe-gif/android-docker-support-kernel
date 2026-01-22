85766fb58b07d5b372ac7c4d7caced2120861b3c
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2026-01-22 19:25:31 +0800

    Update build9.sh

--- scripts/build9.sh
+++ scripts/build9.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
 export ARCH=arm64
-export CLANG_PATH=/home/runner/clang-r353983c/bin
+# export CLANG_PATH=/home/runner/clang-r353983c/bin
+export CLANG_PATH=/home/runner/aarch64-linux-android-4.9/aarch64-linux-android/bin/
+export PATH=$CLANG_PATH:$PATH
+
 
 make O=out my_803sh_defconfig \
     CC=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-android-gcc \
-    AR=llvm-ar \
-    NM=llvm-nm \
-    LD=ld.lld \
-    OBJCOPY=llvm-objcopy \
-    OBJDUMP=llvm-objdump \
-    STRIP=llvm-strip
+    AR=ar \
+    NM=nm \
+    LD=ld \
+    OBJCOPY=objcopy \
+    OBJDUMP=objdump \
+    STRIP=strip
     
 make -j$(nproc --all) O=out \
     CC=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-android-gcc \
-    AR=llvm-ar \
-    NM=llvm-nm \
-    LD=ld.lld \
-    OBJCOPY=llvm-objcopy \
-    OBJDUMP=llvm-objdump \
-    STRIP=llvm-strip
+    AR=ar \
+    NM=nm \
+    LD=ld \
+    OBJCOPY=objcopy \
+    OBJDUMP=objdump \
+    STRIP=strip
     
 if [ -f "out/arch/arm64/boot/Image" ]; then
     echo "✅ 内核编译成功"
