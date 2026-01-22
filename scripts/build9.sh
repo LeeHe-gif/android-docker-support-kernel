2d5f391f90626efff840a7f286ed54ddbdf2fcb9
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2026-01-22 18:02:09 +0800

    Update build9.sh

--- scripts/build9.sh
+++ scripts/build9.sh
@@ -1,21 +1,9 @@
 #!/bin/bash
 
-export CLANG_PREBUILT_BIN=/home/runner/clang-r383902/bin
-export GCC_PREBUILT_BIN=/home/runner/aarch64-linux-android-4.9/bin
-export PATH=$CLANG_PREBUILT_BIN:$GCC_PREBUILT_BIN:$PATH
+export ARCH=arm64
+export GCC_PREBUILT_BIN=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-android-
+export PATH=$GCC_PREBUILT_BIN:$PATH
 
-make O=out my_803sh_defconfig \
-    ARCH=arm64 \
-    CC=clang \
-    CLANG_TRIPLE=$CLANG_TRIPLE \
-    CROSS_COMPILE=$CROSS_COMPILE \
-    LLVM=1 \
-    LLVM_IAS=1
+make O=out my_803sh_defconfig
 
-make -j$(nproc --all) O=out \
-    ARCH=arm64 \
-    CC=clang \
-    CLANG_TRIPLE=$CLANG_TRIPLE \
-    CROSS_COMPILE=$CROSS_COMPILE \
-    LLVM=1 \
-    LLVM_IAS=1
+make -j$(nproc --all) O=out
