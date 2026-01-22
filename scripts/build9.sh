a6e836142d19d026f3288f3ba499a07ba2b5a826
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2026-01-22 18:13:39 +0800

    Enhance build9.sh with ARM make options
    Updated build script to include additional make options and flags for ARM architecture.

--- scripts/build9.sh
+++ scripts/build9.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
-export ARCH=arm64
 export CLANG_PATH=/home/runner/aarch64-linux-android/bin/
 export GCC_PREBUILT_BIN=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export PATH=$CLANG_PATH:$GCC_PREBUILT_BIN:$PATH
 
-make O=out my_803sh_defconfig
+make O=out my_803sh_defconfig \
+    ARCH=arm64 \
+    AR=ar \
+    NM=nm \
+    LD=ld \
+    OBJCOPY=objcopy \
+    OBJDUMP=objdump \
+    STRIP=strip \
+    CROSS_COMPILE=aaarch64-linux-android- \
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
 
-make -j$(nproc --all) O=out
+
+
+make -j$(nproc --all) O=out \
+    ARCH=arm64 \
+    AR=ar \
+    NM=nm \
+    LD=ld \
+    OBJCOPY=objcopy \
+    OBJDUMP=objdump \
+    STRIP=strip \
+    CROSS_COMPILE=aaarch64-linux-android- \
+    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+    KCFLAGS="-Wno-array-bounds -Wformat -Wsometimes-uninitialized -Wformat-extra-args -Wformat-security -Wunknown-warning-option -Wunused-result -Wuninitialized -Wno-error -Wno-pointer-sign"
