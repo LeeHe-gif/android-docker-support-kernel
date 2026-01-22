f2ae84bff5113987c119317977b8f66b255bce14
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2026-01-22 18:07:38 +0800

    Update build9.sh

--- scripts/build9.sh
+++ scripts/build9.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 export ARCH=arm64
+export CLANG_PATH=/home/runner/aarch64-linux-android/bin/
 export GCC_PREBUILT_BIN=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-android-
-export PATH=$GCC_PREBUILT_BIN:$PATH
+export PATH=$CLANG_PATH:$GCC_PREBUILT_BIN:$PATH
 
 make O=out my_803sh_defconfig
 
