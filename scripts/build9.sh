7dda492a01b8656126e3ce39cb50545338e348ff
LeeHe-gif <128717790+LeeHe-gif@users.noreply.github.com>
2026-01-22 19:15:03 +0800

    Update build9.sh

--- scripts/build9.sh
+++ scripts/build9.sh
@@ -2,28 +2,25 @@
 
 export ARCH=arm64
 # export CLANG_PREBUILT_BIN=/home/runner/clang-r353983c/bin
-export CROSS_COMPILE=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 export PATH=/home/runner/aarch64-linux-android-4.9/bin:$PATH
 
-echo "当前工具链:"
-which aarch64-linux-android-gcc
-aarch64-linux-android-gcc --version
-
 make O=out my_803sh_defconfig \
-    AR=aarch64-linux-gnu-ar \
-    NM=aarch64-linux-gnu-nm \
-    LD=aarch64-linux-gnu-ld \
-    OBJCOPY=aarch64-linux-gnu-objcopy \
-    OBJDUMP=aarch64-linux-gnu-objdump \
-    STRIP=aarch64-linux-gnu-strip
+    CC=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-android-gcc \
+    AR=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-gnu-ar \
+    NM=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-gnu-nm \
+    LD=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-gnu-ld \
+    OBJCOPY=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-gnu-objcopy \
+    OBJDUMP=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-gnu-objdump \
+    STRIP=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-gnu-strip
     
 make -j$(nproc --all) O=out \
-    AR=aarch64-linux-gnu-ar \
-    NM=aarch64-linux-gnu-nm \
-    LD=aarch64-linux-gnu-ld \
-    OBJCOPY=aarch64-linux-gnu-objcopy \
-    OBJDUMP=aarch64-linux-gnu-objdump \
-    STRIP=aarch64-linux-gnu-strip
+    CC=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-android-gcc \
+    AR=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-gnu-ar \
+    NM=/home/runner/aarch64-linux-android-4.9/bin/arch64-linux-gnu-nm \
+    LD=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-gnu-ld \
+    OBJCOPY=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-gnu-objcopy \
+    OBJDUMP=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-gnu-objdump \
+    STRIP=/home/runner/aarch64-linux-android-4.9/bin/aarch64-linux-gnu-strip
     
 if [ -f "out/arch/arm64/boot/Image" ]; then
     echo "✅ 内核编译成功"
